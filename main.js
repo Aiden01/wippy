99867ff21c061df7eb3f5732a64b75f1eeedbfac
Elian Gutierrez <eliangu3600103@gmail.com>
2018-08-26 23:27:02 -0400

    fix: Remove useless file

--- main.js
+++
@@ -1,7 +0,0 @@
-function mesAmis(...amis) {
-    console.log(amis) 
-}
-
-mesAmis('WebD', 'quelqu\'un') 
-mesAmis('WebD', 'ddasdsad', 'fsdfsfsdfd', 'dfsdfsdf') 
-mesAmis('WebD', 'fsdfsf', 'fsdfsdfsdfd', 'dsfsdfsdfsdfsf') 
